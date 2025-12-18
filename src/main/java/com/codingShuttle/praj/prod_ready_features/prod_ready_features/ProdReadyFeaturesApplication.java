a3f24c0a1ff737317f7cd688eec10d8267ea5fa7
praj <pritishraj.official@gmail.com>
2025-12-18 23:53:51 +0530

    First Commit - When STS starter and DB connection is done.

---
+++ src/main/java/com/codingShuttle/praj/prod_ready_features/prod_ready_features/ProdReadyFeaturesApplication.java
@@ -0,0 +1,16 @@
+package com.codingShuttle.praj.prod_ready_features.prod_ready_features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdReadyFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdReadyFeaturesApplication.class, args);
+
+		//This project stores details of week 4 of prod ready feature lecture.
+
+	}
+
+}
